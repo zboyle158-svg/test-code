f9b7e6033a2477ebe7f93ae12ac5fecf725d0331
neh793W <2842483372@qq.com>
2026-08-11 19:22:31 +0800

    基础版本BMS

---
+++ BMS/Freertos/BMS_freertos/MDK-ARM/BMS/bms_utils.d
@@ -0,0 +1,3 @@
+bms\bms_utils.o: ..\User\Bms\Core\bms_utils.c
+bms\bms_utils.o: ..\User\Bms\Core\bms_utils.h
+bms\bms_utils.o: I:\keil11\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ BMS/Freertos/BMS_freertos/MDK-ARM/BMS/board.d
@@ -0,0 +1,5 @@
+bms\board.o: ..\User\board.c
+bms\board.o: ..\User\board.h
+bms\board.o: ../User/Drivers/drv_rs485.h
+bms\board.o: ../User/Drivers/drv_can.h
+bms\board.o: I:\keil11\ARM\ARMCC\Bin\..\include\stdint.h
