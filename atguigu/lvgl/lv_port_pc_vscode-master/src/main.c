f3e22cfcf47024be28cb54ca09f8428626e67791
neh793W <2842483372@qq.com>
2026-08-22 10:47:01 +0800

    LVGL第一版

---
+++ atguigu/lvgl/lv_port_pc_vscode-master/src/main.c
@@ -0,0 +1,97 @@
+/**
+ * @file main.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#ifndef _DEFAULT_SOURCE
+  #define _DEFAULT_SOURCE /* needed for usleep() */
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#ifdef _MSC_VER
+  #include <Windows.h>
+#else
+  #include <unistd.h>
+  #include <pthread.h>
+#endif
+#include "lvgl/lvgl.h"
+#include "lvgl/examples/lv_examples.h"
+#include "lvgl/demos/lv_demos.h"
+#include <SDL.h>
+
+#include "hal/hal.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+#if LV_USE_OS != LV_OS_FREERTOS
+
+int main(int argc, char **argv)
+{
+  (void)argc; /*Unused*/
+  (void)argv; /*Unused*/
+
+  /*Initialize LVGL*/
+  lv_init();
+
+  /*Initialize the HAL (display, input devices, tick) for LVGL*/
+  sdl_hal_init(320, 480);
+
+  /* Run the default demo */
+  /* To try a different demo or example, replace this with one of: */
+  /* - lv_demo_benchmark(); */
+  /* - lv_demo_stress(); */
+  /* - lv_example_label_1(); */
+  /* - etc. */
+  lv_demo_widgets();
+
+  while(1) {
+    /* Periodically call the lv_task handler.
+     * It could be done in a timer interrupt or an OS task too.*/
+    uint32_t sleep_time_ms = lv_timer_handler();
+    if(sleep_time_ms == LV_NO_TIMER_READY){
+	sleep_time_ms =  LV_DEF_REFR_PERIOD;
+    }
+#ifdef _MSC_VER
+    Sleep(sleep_time_ms);
+#else
+    usleep(sleep_time_ms * 1000);
+#endif
+  }
+
+  return 0;
+}
+
+
+#endif
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
